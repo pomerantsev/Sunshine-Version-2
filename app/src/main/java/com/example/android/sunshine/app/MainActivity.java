aa2236250b3d9757ce89732fa9b46bb3b7f8d27a
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-11-23 01:30:40 +0300

    Configure periodic sync

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+
 
 public class MainActivity extends ActionBarActivity
         implements ForecastFragment.Callback {
@@ -39,6 +41,8 @@ public class MainActivity extends ActionBarActivity
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -9,6 +9,8 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
  * <p>
@@ -65,6 +67,7 @@ public class SettingsActivity extends PreferenceActivity
             // For other preferences, set the summary to the value's simple string representation.
             preference.setSummary(stringValue);
         }
+        SunshineSyncAdapter.syncImmediately(this);
         return true;
     }
 
